44407718ac723cffd93d2ed7f0964dde13fef110
joshua-angelo <jfouch001@student.franciscan.edu>
2024-02-02 15:03:18 -0500

    Began the start menu! added some music and text color

--- RPGame/GeneralGameFunctions.cpp
+++ RPGame/GeneralGameFunctions.cpp
@@ -5,19 +5,26 @@
 #include <Windows.h>
 
 
+
 void Start_Menu()
 {
-	//play music
+	#pragma comment(lib, "winmm.lib")
+	//play music (ambient music)
+	PlaySound(TEXT("void.wav"), NULL, SND_FILENAME | SND_ASYNC );
 
-	std::cout << "------MOP MANIA------" << std::endl;
+	//for different text colors
+	HANDLE h = GetStdHandle(STD_OUTPUT_HANDLE);
+
 	std::this_thread::sleep_for(std::chrono::seconds(3));
-	std::cout << "You wake up one day with no body... all you see is the void of darkness" << std::endl;
+	std::cout << "\n\nSYou wake up one day with no body... all you see is the void of darkness" << std::endl;
 	std::this_thread::sleep_for(std::chrono::seconds(6));
 	std::cout << "You hear a voice calling in the distance:" << std::endl;
 	std::this_thread::sleep_for(std::chrono::seconds(3));
+	SetConsoleTextAttribute(h, 6); //Janitorius (Tyler) is yellow text
 	std::cout << "My dear soul,";
+	std::cout << " I am Janitorius, the custodian of Le Hospitale." << std::endl;
 	std::this_thread::sleep_for(std::chrono::seconds(4));
-	std::cout << " you seem to have lost all your memory from that day." << std::endl;
+	std::cout << "You seem to have lost all your memory from that day." << std::endl;
 	std::this_thread::sleep_for(std::chrono::seconds(4));
 	std::cout << "You died.";
 	std::this_thread::sleep_for(std::chrono::seconds(3));
@@ -28,5 +35,11 @@ void Start_Menu()
 	std::cout << "A life as a cleaning mop.";
 	std::this_thread::sleep_for(std::chrono::seconds(4));
 	std::cout << " Pretty lame right?" << std::endl;
+
+
+	//end music
+	PlaySound(0, 0, 0);
+	//end text color
+	SetConsoleTextAttribute(h, 7);
 
 }
