dffda47940fa84feb1c455886eabe6d82bbd8f2d
Shazookie <143617061+Shazookie@users.noreply.github.com>
2024-04-18 20:21:11 -0400

    errrors

--- RPGame/SeamusEvent.cpp
+++ RPGame/SeamusEvent.cpp
@@ -14,6 +14,161 @@ void cellarEvent::trigger(character& c)
 {
 	if (this->isDone == true) {
 
+		while(gold>0){
+
+cout<<"\nThis is your total remaining gold:\n"<<gold<<"g";
+cout<<"\n\n\tType the corresponding number for the catagory of wares you'd like to browse\n";
+cout<<"1. Armor\n";
+cout<<"2. Weapons\n";
+cout<<"3. Spells\n";
+cout<<"4. Potions\n";
+cout<<"5. Exit shop\n";
+int choice;
+cin>>choice;
+if (choice==5){
+    cout<<"Thank you come again";
+    break;}
+
+ if(choice !=1 && choice !=2 && choice !=3 && choice !=4){
+    cout<<"you bum, thats not even an option! pick a number between 1 and 4\n";}
+switch(choice)
+
+{
+    case 1:
+    cout<<"\n\tType the corresponding number of the armor you would like to purchase\n";
+    cout<<"1. Thunderhelm(25g)\n";
+    cout<<"2. Arch Mage's Robes(50G)\n";
+    cout<<"3. Hover Boots(100G)\n";
+    cout<<"4. Diamond leggings(500G)\n";
+    int choiceA;
+    cin>>choiceA;
+        if(choiceA !=1 && choiceA !=2 && choiceA !=3 && choiceA !=4){
+    cout<<"you bum, thats not even an option! pick a number between 1 and 4";}
+    switch(choiceA){
+    case 1:
+    cout<<"thanks for purchasing the Thunderhelm for 25 gold\n";
+     gold=TWEN(gold, price);
+      break;
+    case 2:
+    cout<<"thanks for purchasing the Arch Mage's Robes for 50 gold\n";
+     gold=FIFT(gold, price);
+      break;
+    case 3:
+    cout<<"thanks for purchasing the Hover Boots for 100 gold\n";
+     gold=HUND(gold, price);
+      break;
+    case 4:
+    cout<<"thanks for purchasing the Diamond Leggings for 500 gold\n";
+    gold=FHUND(gold, price);
+      break;
+
+    }
+    return main();
+    break;
+
+    case 2:
+    cout<<"\n\tType the corresponding number of the weapon you would like to purchase\n";
+    cout<<"1. Death Note(25g)\n";
+    cout<<"2. Raygun(50G)\n";
+    cout<<"3. Monado(100G)\n";
+    cout<<"4. Stanley's Bucket(500G)\n";
+    int choiceB;
+    cin>>choiceB;
+     if(choiceB !=1 && choiceB !=2 && choiceB !=3 && choiceB !=4){
+    cout<<"you bum, thats not even an option! pick a number between 1 and 4";}
+    switch(choiceB){
+   
+    case 1:
+    cout<<"thanks for purchasing the Death Note for 25 gold\n";
+        gold=TWEN(gold, price);
+      break;
+    case 2:
+    cout<<"thanks for purchasing the Raygun for 50 gold\n";
+        gold=FIFT(gold, price);
+      break;
+    case 3:
+    cout<<"thanks for purchasing the Monado for 100 gold\n";
+        gold=HUND(gold, price);
+      break;
+    case 4:
+    cout<<"thanks for purchasing Stanley's Bucket for 500 gold\n";
+        gold=FHUND(gold, price);
+      break;
+    }
+     return main();
+    break;
+
+ case 3:
+    cout<<"\n\tType the corresponding number of the Spell Book you would like to purchase\n";
+    cout<<"1. Spell Book of Electro-telekinis(25g)\n";
+    cout<<"2. Spell Book of Extreme Flatuation(50g)\n";
+    cout<<"3. Spell Book of Instant Time Stop(100G)\n";
+    cout<<"4. Spell Book of Object oriented programming(500g)\n";
+    int choiceC;
+    cin>>choiceC;
+     if(choiceC !=1 && choiceC !=2 && choiceC !=3 && choiceC !=4){
+    cout<<"you bum, thats not even an option! pick a number between 1 and 4";}
+    switch(choiceC){
+   
+    case 1:
+    cout<<"thanks for purchasing the Spell Book of Electro-telekinis for 25 gold\n";
+        gold=TWEN(gold, price);
+      break;
+    case 2:
+    cout<<"thanks for purchasing the Spell Book of Extreme Flatuation for 50 gold\n";
+        gold=FIFT(gold, price);
+      break;
+    case 3:
+    cout<<"thanks for purchasing the Spell Book of Instant Time Stop for 100 gold\n";
+        gold=HUND(gold, price);
+      break;
+    case 4:
+    cout<<"thanks for purchasing the Spell Book of Ojbcect oriented programming for 500 gold\n";
+        gold=FHUND(gold, price);
+      break;
+    }
+    return main();
+    break;
+
+     case 4:
+    cout<<"\n\tType the corresponding number of the Potion you would you like to purchase\n";
+    cout<<"1. Potion of Instant Death(25g)\n";
+    cout<<"2. Juggernog (50g)\n";
+    cout<<"3. Felix Felicis (100g)\n";
+    cout<<"4. Ohio Water Potion (500g)\n";
+    int choiceD;
+    cin>>choiceD;
+     if(choiceD !=1 && choiceD !=2 && choiceD !=3 && choiceD !=4){
+    cout<<"you bum, thats not even an option! pick a number between 1 and 4";}
+    switch(choiceD){
+   
+    case 1:
+    cout<<"thanks for purchasing a Potion of Instant Death for 25 gold(kills anyone who touches the vial)\n";
+        gold=TWEN(gold, price);
+      break;
+    case 2:
+    cout<<"thanks for purchasing a bottle of Juggernog for 50 gold\n";
+        gold=FIFT(gold, price);
+      break;
+    case 3:
+    cout<<"thanks for purchasing a vial of Felix Felicis for 100 gold\n";
+        gold=HUND(gold, price);
+      break;
+    case 4:
+    cout<<"thanks for purchasing a botte of Ohio Water for 500 gold\n";
+        gold=FHUND(gold, price);
+      break;
+    }
+    return main();
+    break;
+
+}
+}
+if (gold==0){
+    cout<<"get outta here ya broke boi!!!";
+    return 0;}
+return 0;}
+
 		return;	//if character has already triggered the event in this node
 	}
 	else {
