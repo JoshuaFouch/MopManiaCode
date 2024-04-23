0c7f66c3c97313ca90ef325acab2c0a0ca127c9e
joshua-angelo <jfouch001@student.franciscan.edu>
2024-04-23 18:15:51 -0400

    jhifjy

--- RPGame/SeamusStuff.cpp
+++ RPGame/SeamusStuff.cpp
@@ -3,192 +3,100 @@
 #include "character.h"
 #include "battle.h"
 #include "npc.h"
-//#include <ChatGPTsnake.h>
+
 
 using namespace std;
-
-/*
-"; MSdelay(200); cout << "s"; MSdelay(200); cout << "s"; MSdelay(200); cout << "s"; MSdelay(200); cout << "
-c.Rat_Sale(4);
-*/
-
-
-/*
-    srand(time(NULL));
-    Setup();
-    while (!gameOver)
-    {
-        Draw();
-        Input();
-        Logic();
-        usleep(100000); // Sleep for 0.1 seconds
-    }
-    return 0;
-
-
-*/
 
 cellarEvent::cellarEvent() {};
 
 void cellarEvent::trigger(character& c)
 {
-    if (this->isDone == true) {
-
+    if (this->isDone != true) {
         endMusic();
         playMusic("putrid.wav");
 
-        while (c.get_deadRat() >= 4) {  //while you still have enough rats to buy things
-            color(5);
-            cout << "\nYou have " << c.get_deadRat() << " rats"; MSdelay(500); cout << "s"; MSdelay(500); cout << "s"; MSdelay(500); cout << "s"; MSdelay(500); cout << " remaining:" << endl;
-            cout << "\n\n\tWhat type of items"; MSdelay(100); cout << "s"; MSdelay(100); cout << "s"; MSdelay(100); cout << "s"; MSdelay(100); cout << " would you like?\n";
-            color(5); std::cout << "<~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~{" << std::endl;
-            color(5); std::cout << "|"; color(8); std::cout << "[1]: "; color(7); std::cout << "Potions" << std::endl;
-            color(5); std::cout << "|"; color(8); std::cout << "[2]: "; color(7); std::cout << "Specialty Items" << std::endl;	//janitorius is the same person as grime reaper, no one knows who janitorius is except u
-            color(5); std::cout << "|"; color(8); std::cout << "[3]: "; color(7); std::cout << "Play Game" << std::endl;// neat? not my code tho
-            color(5); std::cout << "|"; color(8); std::cout << "[4]: "; color(7); std::cout << "Exit" << std::endl;
-            color(5); std::cout << "<~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~{" << std::endl;
-
-            int choice;
-            cin >> choice;
-            if (choice == 3) {
+        if (c.get_deadRat() < 4) {
+            playSFX("vine-boom.wav");
+            cout << "get outta here ya broke boy!!!";   //will fix this and the many other errors Iv've enevitibly made
+            MSdelay(5000);
+            return;
+        }
+        else {
+            while (true) {
                 color(5);
-                cout << "I'll s"; MSdelay(200); cout << "s"; MSdelay(200); cout << "s"; MSdelay(200); cout << "s"; MSdelay(200); cout << "ee you again s"; MSdelay(200); cout << "s"; MSdelay(200); cout << "s"; MSdelay(200); cout << "s"; MSdelay(200); cout << "oon" << endl; MSdelay(4000);
-                endMusic();
-                break;
-            }
+                clear();
+                cout << "You have " << c.get_deadRat() << " rats"; MSdelay(400); cout << "s"; MSdelay(400); cout << "s"; MSdelay(400); cout << "s"; MSdelay(400); cout << " remaining:" << endl;
+                cout << "\n\tWhat type of items"; MSdelay(400); cout << "s"; MSdelay(300); cout << "s"; MSdelay(200); cout << "s"; MSdelay(100); cout << " would you like?\n";
+                color(5); std::cout << "<~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~{" << std::endl;
+                color(5); std::cout << "|"; color(8); std::cout << "[1]: "; color(7); std::cout << "Potionsss" << std::endl;
+                color(5); std::cout << "|"; color(8); std::cout << "[2]: "; color(7); std::cout << "Specialty Itemsss" << std::endl;
+                color(5); std::cout << "|"; color(8); std::cout << "[3]: "; color(7); std::cout << "Play Game" << std::endl;// neat? not my code tho
+                color(5); std::cout << "|"; color(8); std::cout << "[4]: "; color(7); std::cout << "Exit" << std::endl;
+                color(5); std::cout << "<~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~{" << std::endl;
 
-            if (choice != 1 && choice != 2 && choice != 3) {
-                color(5);
-                cout << "You s"; MSdelay(200); cout << "s"; MSdelay(200); cout << "s"; MSdelay(200); cout << "s"; MSdelay(200); cout << "tupid s"; MSdelay(200); cout << "s"; MSdelay(200); cout << "s"; MSdelay(200); cout << "s"; MSdelay(200); cout << "ud, thats"; MSdelay(200); cout << "s"; MSdelay(200); cout << "s"; MSdelay(200); cout << "s"; MSdelay(200); cout << " not even an option\n";
-            }
-            switch (choice)
-            {
-        
+                int choice;
+                cin >> choice;
+
+                switch (choice) {
                 case 1:
-                    color(5);
-                    clear();
-                    cout << "\n\tWhat potions"; MSdelay(200); cout << "s"; MSdelay(200); cout << "s"; MSdelay(200); cout << "s"; MSdelay(200); cout << " would you like?\n";
-                    color(5); std::cout << "<~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~{" << std::endl;
-                    color(5); std::cout << "|"; color(8); std::cout << "[1]: "; color(7); std::cout << "Sponge armor (increases your character's HP) - 4 rats" << '\n';
-                    color(5); std::cout << "|"; color(8); std::cout << "[2]: "; color(7); std::cout << "Bleach lining (increases your character's ATK) - 4 rats" << '\n';
-                    color(5); std::cout << "|"; color(8); std::cout << "[3]: "; color(7); std::cout << "Wax-coat (increases your character's DEF) - 4 rats" << '\n';
-                    color(5); std::cout << "|"; color(8); std::cout << "[4]: "; color(7); std::cout << "Back to main shop" << std::endl;
-                    color(5); std::cout << "<~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~{" << std::endl;
-                    int choiceA;
-                    cin >> choiceA;
-                    if (choiceA != 1 && choiceA != 2 && choiceA != 3) {
-                        cout << "You s"; MSdelay(200); cout << "s"; MSdelay(200); cout << "s"; MSdelay(200); cout << "s"; MSdelay(200); cout << "tupid s"; MSdelay(200); cout << "s"; MSdelay(200); cout << "s"; MSdelay(200); cout << "s"; MSdelay(200); cout << "ud, thats"; MSdelay(200); cout << "s"; MSdelay(200); cout << "s"; MSdelay(200); cout << "s"; MSdelay(200); cout << " not even an option\n";
-                    }
-                    switch (choiceA) {
-                    case 1:
+                    while (true) {
                         color(5);
-                        cout << "Pleasure doing busines"; MSdelay(200); cout << "s"; MSdelay(200); cout << "s"; MSdelay(200); cout << "s"; MSdelay(200); cout << "\n";
-                        cout << "Here's"; MSdelay(200); cout << "s"; MSdelay(200); cout << "s"; MSdelay(100); cout << "s"; MSdelay(100); cout << " your s"; MSdelay(200); cout << "s"; MSdelay(100); cout << "s"; MSdelay(100); cout << "s"; MSdelay(200); cout << "ponge armor\n";
-                        c.Rat_Sale(4);
-                        c.get_healUp();
-                        break;
-                    case 2:
-                        color(5);
-                        cout << "Pleasure doing busines"; MSdelay(200); cout << "s"; MSdelay(200); cout << "s"; MSdelay(200); cout << "s"; MSdelay(200); cout << "\n";
-                        cout << "Here's"; MSdelay(200); cout << "s"; MSdelay(200); cout << "s"; MSdelay(100); cout << "s"; MSdelay(100); cout << " your Bleach lining potion\n";
-                        c.Rat_Sale(4);
-                        c.get_attUp();
-                        break;
-                    case 3:
-                        color(5);
-                        cout << "Pleasure doing busines"; MSdelay(200); cout << "s"; MSdelay(200); cout << "s"; MSdelay(200); cout << "s"; MSdelay(200); cout << "\n";
-                        cout << "Here's"; MSdelay(200); cout << "s"; MSdelay(200); cout << "s"; MSdelay(100); cout << "s"; MSdelay(100); cout << " your Wax"; MSdelay(200); cout << "s"; MSdelay(200); cout << "s"; MSdelay(100); cout << "s"; MSdelay(100); cout << " coat\n";
-                        c.Rat_Sale(4);
-                        c.get_defUp();
-                        break;
-                    }
+                        clear();
+                        cout << "\t\tWhat potions"; MSdelay(200); cout << "s"; MSdelay(200); cout << "s"; MSdelay(200); cout << "s"; MSdelay(200); cout << " would you like?\n";
+                        color(5); std::cout << "<~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~{" << std::endl;
+                        color(5); std::cout << "|"; color(8); std::cout << "[1]: "; color(7); std::cout << "Sponge armor (increases your HP) - 4 ratsss" << '\n';
+                        color(5); std::cout << "|"; color(8); std::cout << "[2]: "; color(7); std::cout << "Bleach lining (increases your ATK) - 4 ratsss" << '\n';
+                        color(5); std::cout << "|"; color(8); std::cout << "[3]: "; color(7); std::cout << "Wax-coat (increases your DEF) - 4 ratsss" << '\n';
+                        color(5); std::cout << "|"; color(8); std::cout << "[4]: "; color(7); std::cout << "Back to main shop" << std::endl;
+                        color(5); std::cout << "<~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~{" << std::endl;
 
-                    break;
+                        int choiceA;
+                        cin >> choiceA;
+
+                        switch (choiceA)
 
                 case 2:
-                    color(5);
-                    clear();
-                    cout << "\n\tSpecialty items"; MSdelay(200); cout << "s"; MSdelay(200); cout << "s"; MSdelay(200); cout << "s"; MSdelay(200); cout << " would you like?\n";
-                    color(5); std::cout << "<~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~{" << std::endl;
-                    color(5); std::cout << "|"; color(8); std::cout << "[1]: "; color(7); std::cout << "Slippery Soap (lets you escape any battle) - 8 rats" << '\n';
-                    color(5); std::cout << "|"; color(8); std::cout << "[2]: "; color(7); std::cout << "Cleaning tips (showsss enemy'sss current statsss) - 4 rats" << '\n';//new one to show you the stats of your enemy?
-                    color(5); std::cout << "|"; color(8); std::cout << "[3]: "; color(7); std::cout << "Whendex (increasesss each max stat by 5) - 10 rats" << '\n';//shows how many dead rats you have collected and how many are left maybe? the just when you use the item in inventory this info is displayed
-                    color(5); std::cout << "|"; color(8); std::cout << "[4]: "; color(7); std::cout << "Mirror (lets you see yourself as others see you) - 4 rats" << '\n';//shows how many dead rats you have collected and how many are left maybe? the just when you use the item in inventory this info is 
-                    color(5); std::cout << "|"; color(8); std::cout << "[5]: "; color(7); std::cout << "Back to main shop" << std::endl;
-                    color(5); std::cout << "<~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~{" << std::endl;
-                    int choiceB;
-                    cin >> choiceB;
-                    if (choiceB != 1 && choiceB != 2 && choiceB != 3 && choiceB != 4) {
-                        cout << "You s"; MSdelay(200); cout << "s"; MSdelay(200); cout << "s"; MSdelay(200); cout << "s"; MSdelay(200); cout << "tupid s"; MSdelay(200); cout << "s"; MSdelay(200); cout << "s"; MSdelay(200); cout << "s"; MSdelay(200); cout << "ud, thats"; MSdelay(200); cout << "s"; MSdelay(200); cout << "s"; MSdelay(200); cout << "s"; MSdelay(200); cout << " not even an option\n";
-                    }
-                    switch (choiceB) {
-
-                    case 1:
-                        color(5);
-                        cout << "Pleasure doing busines"; MSdelay(200); cout << "s"; MSdelay(200); cout << "s"; MSdelay(200); cout << "s"; MSdelay(200); cout << "\n";
-                        cout << "Here's"; MSdelay(200); cout << "s"; MSdelay(200); cout << "s"; MSdelay(100); cout << "s"; MSdelay(100); cout << " your S"; MSdelay(200); cout << "s"; MSdelay(200); cout << "s"; MSdelay(200); cout << "s"; MSdelay(200); cout << "lippery S"; MSdelay(200); cout << "s"; MSdelay(200); cout << "s"; MSdelay(200); cout << "s"; MSdelay(200); cout << "oap\n";
-                        c.Rat_Sale(4);
-                        c.acq_slipperySoap();// will have to make these
-                        MSdelay(2222);
-                        break;
-                    case 2:
-                        color(5);
-                        cout << "Pleasure doing busines"; MSdelay(200); cout << "s"; MSdelay(200); cout << "s"; MSdelay(200); cout << "s"; MSdelay(200); cout << "\n";
-                        cout << "Here's"; MSdelay(200); cout << "s"; MSdelay(200); cout << "s"; MSdelay(100); cout << "s"; MSdelay(100); cout << " your Cleaning tips"; MSdelay(200); cout << "s"; MSdelay(200); cout << "s"; MSdelay(200); cout << "s"; MSdelay(200); cout << "\n";
-                        c.Rat_Sale(4);
-                        c.acq_cleaningTips();
-                        MSdelay(2222);
-                        break;
-                    case 3:
-                        color(5);
-                        cout << "Pleasure doing busines"; MSdelay(200); cout << "s"; MSdelay(200); cout << "s"; MSdelay(200); cout << "s"; MSdelay(200); cout << "\n";
-                        cout << "Here's"; MSdelay(200); cout << "s"; MSdelay(200); cout << "s"; MSdelay(100); cout << "s"; MSdelay(100); cout << " your Whendex"; MSdelay(200); cout << "s"; MSdelay(200); cout << "s"; MSdelay(200); cout << "s"; MSdelay(200); cout << "\n";
-                        c.Rat_Sale(4);
-                        c.acq_Whendex();
-                        MSdelay(2222);
-                        break;
-
-                    case 4:
-                        color(5);
-                        cout << "Pleasure doing busines"; MSdelay(200); cout << "s"; MSdelay(200); cout << "s"; MSdelay(200); cout << "s"; MSdelay(200); cout << "\n";
-                        cout << "Here's"; MSdelay(200); cout << "s"; MSdelay(200); cout << "s"; MSdelay(100); cout << "s"; MSdelay(100); cout << " your Mirror"; MSdelay(200); cout << "\n";
-                        c.Rat_Sale(4);
-                        c.acq_Mirror();
-                        MSdelay(2222);
-                        break;
-                    }
-                    break;
-
                 case 3:
-                    /* srand(time(NULL));
-                 Setup();
-                 while (!gameOver)
-                 {
-                     Draw();
-                     Input();
-                     Logic();
-                     usleep(100000); // Sleep for 0.1 seconds
-                 }
-                   break;//when you lose or leave the game it just resets
-                 }
-                 return main();
-                 break;*/
-                    std::cout << " this will be game" << std::endl;
-            }
-            if (c.get_deadRat() < 4) {
-                playSFX("vine-boom.wav");
-                cout << "get outta here ya broke boi!!!";   //will fix this and the many other errors Iv've enevitibly made
-                MSdelay(5000);
-                return;
-            }
+                case 4:
+                    color(5);
+                    cout << "I'll s"; MSdelay(200); cout << "s"; MSdelay(200); cout << "s"; MSdelay(200); cout << "s"; MSdelay(200); cout << "ee you again s"; MSdelay(200); cout << "s"; MSdelay(200); cout << "s"; MSdelay(200); cout << "s"; MSdelay(200); cout << "oon" << endl; MSdelay(4000);
+                    endMusic();
+                    break;
+                default:
+                    color(5);
+                    cout << "You s"; MSdelay(200); cout << "s"; MSdelay(200); cout << "s"; MSdelay(200); cout << "s"; MSdelay(200); cout << "tupid s"; MSdelay(200); cout << "s"; MSdelay(200); cout << "s"; MSdelay(200);
+                    cout << "s"; MSdelay(200); cout << "ud, thats"; MSdelay(200); cout << "s"; MSdelay(200); cout << "s"; MSdelay(200); cout << "s"; MSdelay(200); cout << " not even an option\n";
+                    MSdelay(3200);
+                    }
+
+                }
         }
+
+
+        
+       
+ 
     }
      else {
          Sdelay(2);
          std::cout << "You entered The Cellar... ";
          MSdelay(3000);
-         std::cout << "The room is dark, and the air thick as mud, with a wretched stench of rotting flesh..." << std::endl;
+         std::cout << "The room is dark, and the air thick as mud, with a wretched stench of rotting flesh"; ellipsis(); std::cout << endl;
          MSdelay(4000);
+         std::cout << "What is this?? You see a rat on the ground!!" << std::endl;
+         playSFX("BAH.wav");
+         MSdelay(4500);
+         std::cout << "Oh its dead... you decided to pick it up..." << std::endl;
+         MSdelay(2400);
+         c.acq_deadRat();
+         Sdelay(4);
+         clear();
+         color(5);
+         std::cout << "hss"; cout << "s"; MSdelay(200); cout << "s"; MSdelay(200); cout << "s" << endl; MSdelay(200);
+         Sdelay(3);
+         color(7);
+         cout << "WHAT WAS THAT???" << endl;
+         Sdelay(2);
          std::cout << "You walk towards the back of the room and can faintly see a lengthy crack in the wall..." << std::endl;
          MSdelay(5000);
          std::cout << "Suddenly you see a pair slitted eyes blink open, their yellow glow slightly lighting up the room " << std::endl;
@@ -221,11 +129,11 @@ void cellarEvent::trigger(character& c)
          ellipsis();
          clear();
          MSdelay(1300);
-         cout << "s"; MSdelay(200); cout << "s"; MSdelay(200); cout << "s"; MSdelay(200); cout << "s"; MSdelay(500); cout << "s"; MSdelay(200); cout << "s"; MSdelay(200); cout << "s"; MSdelay(200); cout << "s"; MSdelay(500); cout << "s"; MSdelay(200); cout << "s"; MSdelay(200); cout << "s"; MSdelay(200); cout << "s"; MSdelay(500); cout << "s"; MSdelay(200); cout << "s"; MSdelay(200); cout << "s"; MSdelay(200); cout << "s"; MSdelay(500); cout << "s"; MSdelay(200); cout << "s"; MSdelay(200); cout << "s"; MSdelay(200); cout << "s"; MSdelay(500);
+         cout << "s"; MSdelay(200); cout << "s"; MSdelay(200);  cout << "s"; MSdelay(200); cout << "s"; MSdelay(200);
          MSdelay(5000);
          endMusic();
 
-         this->isDone = true;
+         //this->isDone = true;
          return;
      }
 }
